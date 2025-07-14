d157011696819ea4a3089e3a82f92efcb4a2bf19
phungpdhe189026 <pdphung27@gmail.com>
2025-07-14 19:35:55 +0700

    phungpd

--- Presentation Layer/App.xaml.cs
+++ Presentation Layer/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Presentation_Layer
 {
@@ -9,6 +10,26 @@ namespace Presentation_Layer
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var settings = AppSettings.Load();
+
+            if (settings.Theme == "Dark")
+            {
+                Application.Current.Resources["WindowBackgroundColor"] = new SolidColorBrush(Color.FromRgb(30, 30, 30));
+                Application.Current.Resources["TextForegroundColor"] = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                Application.Current.Resources["WindowBackgroundColor"] = new SolidColorBrush(Color.FromRgb(244, 246, 248));
+                Application.Current.Resources["TextForegroundColor"] = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+
     }
+
 
 }
