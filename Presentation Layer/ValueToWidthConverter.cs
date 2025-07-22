179150e4104c76749d4fa639d7f02b76c3fbaa89
TienTD186397 <tientdhe186397@fpt.edu.vn>
2025-07-23 06:06:18 +0700

    Admin Instructor

---
+++ Presentation Layer/ValueToWidthConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Presentation_Layer
+{
+    public class ValueToWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double d)
+            {
+                double scaled = Math.Min(d * 2, 200);
+                return Math.Max(scaled, 5);
+            }
+            return 5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
